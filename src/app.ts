433c71e3ec4c5ae6f323b2f903e32cba1f2adaad
Brendo Silva <breendo.01@gmail.com>
2021-10-19 14:23:36 -0300

    service authenticate github e controller

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,20 @@
+import "dotenv/config";
 import express from "express";
+import { router } from "./routes";
 
 const app = express();
+
+app.use(router)
+
+app.get("/github", (request, response) => {
+  response.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`)
+});
+
+app.get("/signin/cb", (request, response) => {
+  const { code } = request.query;
+
+  return response.json({ code: code})
+})
 
 app.listen(4000, () => {
   console.log(`🚀 - server is running on PORT 4000`);
